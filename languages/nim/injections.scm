f539b578c7fbdafcbb58724d522553603cc53621
Igor Maljuk <tigormal@gmail.com>
2024-11-02 03:08:56 +0100

    Fixes for language injections

--- languages/nim/injections.scm
+++ languages/nim/injections.scm
@@ -4,16 +4,16 @@
 (generalized_string
   function: (identifier) @_string_prefix
   .
-  (string_content) @injection.content
-  (#set! injection.language "regex")
+  (string_content) @content
+  (#set! "language" "regex")
   (#any-of? @_string_prefix "re" "rex"))
 
 ; format string in generalized_strings
 (generalized_string
   function: (identifier) @_string_prefix
   .
-  (string_content) @injection.content
-  (#set! injection.language "nim_format_string")
+  (string_content) @content
+  (#set! "language" "nim_format_string")
   (#eq? @_string_prefix "fmt"))
 
 ; format string in normal strings with & prefix
@@ -21,16 +21,61 @@
   operator: (operator) @_string_prefix
   .
   (_
-    (string_content) @injection.content)
-  (#set! injection.language "nim_format_string")
+    (string_content) @content)
+  (#set! "language" "nim_format_string")
   (#eq? @_string_prefix "&"))
 
 ; sql in generalized_strings
 ; and anything you like as long as the function name is the same as the injected language's parser
+
+; TODO: this doesn't work dynamically, must be a workaround
+; (generalized_string
+;   function: (identifier) @injection.language
+;   (string_content) @content
+;   (#not-any-of? @injection.language "re" "rex" "fmt"))
+
+; SQL, HTML, CSS, XML, GLSL and Markdown
 (generalized_string
-  function: (identifier) @injection.language
-  (string_content) @injection.content
-  (#not-any-of? @injection.language "re" "rex" "fmt"))
+  function: (identifier) @_string_prefix
+  .
+  (string_content) @content
+  (#set! "language" "sql")
+  (#eq? @_string_prefix "sql"))
+
+(generalized_string
+  function: (identifier) @_string_prefix
+  .
+  (string_content) @content
+  (#set! "language" "html")
+  (#eq? @_string_prefix "html"))
+
+(generalized_string
+  function: (identifier) @_string_prefix
+  .
+  (string_content) @content
+  (#set! "language" "css")
+  (#eq? @_string_prefix "css"))
+
+(generalized_string
+  function: (identifier) @_string_prefix
+  .
+  (string_content) @content
+  (#set! "language" "xml")
+  (#eq? @_string_prefix "xml"))
+
+(generalized_string
+  function: (identifier) @_string_prefix
+  .
+  (string_content) @content
+  (#set! "language" "glsl")
+  (#eq? @_string_prefix "glsl"))
+
+(generalized_string
+  function: (identifier) @_string_prefix
+  .
+  (string_content) @content
+  (#set! "language" "markdown")
+  (#eq? @_string_prefix "md"))
 
 ; =============================================================================
 ; emit pragma
@@ -45,7 +90,8 @@
 ; {.emit: "<javascript code>".}
 ; normal strings
 ((comment
-  (comment_content) @injection.language)
+  (comment_content) @_language
+  (#eq? @_language "cpp"))
   .
   (pragma_statement
     (pragma_list
@@ -53,27 +99,65 @@
         left: (identifier) @_emit_keyword
         (#eq? @_emit_keyword "emit")
         right: (_
-          (string_content) @injection.content)))))
+          (string_content) @content))))
+  (#set! "language" "cpp"))
+
+((comment
+  (comment_content) @_language
+  (#eq? @_language "c"))
+  .
+  (pragma_statement
+    (pragma_list
+      (colon_expression
+        left: (identifier) @_emit_keyword
+        (#eq? @_emit_keyword "emit")
+        right: (_
+          (string_content) @content))))
+  (#set! "language" "c"))
+
+((comment
+  (comment_content) @_language
+  (#eq? @_language "objc"))
+  .
+  (pragma_statement
+    (pragma_list
+      (colon_expression
+        left: (identifier) @_emit_keyword
+        (#eq? @_emit_keyword "emit")
+        right: (_
+          (string_content) @content))))
+  (#set! "language" "objc"))
+
+((comment
+  (comment_content) @_language
+  (#eq? @_language "javascript"))
+  .
+  (pragma_statement
+    (pragma_list
+      (colon_expression
+        left: (identifier) @_emit_keyword
+        (#eq? @_emit_keyword "emit")
+        right: (_
+          (string_content) @content))))
+  (#set! "language" "javascript"))
+
 
 ; =============================================================================
 ; asm statement
-; works same as emit pragma, needs preceding comment with language name
-((comment
-  (comment_content) @injection.language)
-  .
-  (assembly_statement
+((assembly_statement
     (_
-      (string_content) @injection.content)))
+      (string_content) @content))
+  (#set! "language" "asm"))
 
 ; =============================================================================
 ; comments
 ; NOTE: ts "comment" parser heavily impacts performance
 ; markdown parser in documentation_comment
 (documentation_comment
-  (comment_content) @injection.content
-  (#set! injection.language "markdown_inline"))
+  (comment_content) @content
+  (#set! "language" "markdown_inline"))
 
 ; markdown parser in block_documentation_comment
 (block_documentation_comment
-  (comment_content) @injection.content
-  (#set! injection.language "markdown"))
+  (comment_content) @content
+  (#set! "language" "markdown"))
