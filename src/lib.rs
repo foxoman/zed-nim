ccca0c2da23f6e2ed714455e37e46bab9462dbf2
Igor Maljuk <tigormal@proton.me>
2025-02-28 06:13:26 +0100

    Added settings support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use zed_extension_api as zed;
-
+use zed::settings::LspSettings;
+use zed_extension_api::{self as zed, Result};
 struct NimExtension {}
 
 impl zed::Extension for NimExtension {
@@ -12,6 +12,24 @@ impl zed::Extension for NimExtension {
         _language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> zed::Result<zed::Command> {
+        // Get configuration from settings
+        let binary_settings = LspSettings::for_worktree("nim", worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.binary);
+
+        let binary_args = binary_settings
+            .as_ref()
+            .and_then(|binary_settings| binary_settings.arguments.clone());
+
+        if let Some(path) = binary_settings.and_then(|binary_settings| binary_settings.path) {
+            return Ok(zed::Command {
+                command: path,
+                args: binary_args.unwrap_or_default(),
+                env: Default::default(),
+            });
+        }
+
+        // If no setting specified, use the default
         let path = worktree
             .which("nimlangserver")
             .ok_or_else(|| "nim-langserver not installed.".to_string())?;
@@ -21,6 +39,21 @@ impl zed::Extension for NimExtension {
             args: vec![],
             env: Default::default(),
         })
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<zed::serde_json::Value>> {
+        let settings = LspSettings::for_worktree("nim", worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+
+        Ok(Some(zed::serde_json::json!({
+            "nim": settings
+        })))
     }
 }
 
